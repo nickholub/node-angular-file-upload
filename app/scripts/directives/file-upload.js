6b91b99b73c424ef1a730fed8586f12215c6495f
Nick Holub <nikholub@gmail.com>
2014-09-08 14:40:25 -0700

    file upload directive

---
+++ app/scripts/directives/file-upload.js
@@ -0,0 +1,70 @@
+'use strict';
+
+angular.module('ui.fileUpload', ['angularFileUpload'])
+  .directive('fileUpload', function (FileUploader, $timeout) {
+    return {
+      restrict: 'A',
+      replace: true,
+      templateUrl: 'scripts/directives/file-upload.html',
+      scope: true,
+      link: function (scope, element, attrs) {
+        var options = scope.$eval(attrs.fileUpload);
+
+        console.log(options);
+
+        if (!options || !options.url) {
+          throw 'Upload URL is not set';
+        }
+
+        var uploader = new FileUploader({
+          url: options.url
+        });
+        scope.uploader = uploader;
+
+        scope.uploading = false;
+
+        angular.extend(uploader, {
+          onAfterAddingFile: function () {
+            scope.alert = null;
+            scope.uploading = true;
+            uploader.uploadAll();
+          },
+          onProgressAll: function (progress) {
+            scope.progress = progress;
+          },
+          onSuccessItem: function (fileItem) {
+            scope.uploading = false;
+
+            scope.alert = {
+              type: 'success',
+              msg: 'File ' + fileItem.file.name + ' is successfully uploaded'
+            };
+
+            if (options.onSuccess) {
+              options.onSuccess(fileItem);
+            }
+          },
+          onErrorItem: function (fileItem, response, status) {
+            console.log(fileItem);
+            scope.uploading = false;
+
+            scope.alert = {
+              type: 'danger',
+              msg: 'File ' + fileItem.file.name + ' upload failed',
+              details: status + ' ' + response
+            };
+          }
+        });
+
+        scope.closeAlert = function () {
+          scope.alert = null;
+        };
+
+        scope.selectFile = function () {
+          $timeout(function () {
+            element.find('input').trigger('click');
+          }, 0);
+        };
+      }
+    };
+  });
