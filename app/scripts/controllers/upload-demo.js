55eb97e811d819e6a77c853e70240e4b376f2576
Nick Holub <nikholub@gmail.com>
2014-09-08 17:01:20 -0700

    Upload Log

--- app/scripts/controllers/upload-demo.js
+++ app/scripts/controllers/upload-demo.js
@@ -1,14 +1,31 @@
 'use strict';
 
 angular.module('app')
-  .controller('UploadDemoCtrl', function ($scope) {
+  .controller('UploadDemoCtrl', function ($scope, FileUploadModal) {
+    $scope.alerts = [];
+
     $scope.fileUploadOptions = {
       url: '/upload',
       success: function (fileItem) {
-        console.log('uploaded ' + fileItem.file.name);
+        $scope.alerts.push({
+          type: 'success',
+          msg: 'uploaded ' + fileItem.file.name
+        });
       },
       error: function (fileItem) {
-        console.log('failed ' + fileItem.file.name);
+        $scope.alerts.push({
+          type: 'danger',
+          msg: 'failed ' + fileItem.file.name
+        });
       }
+    };
+
+    $scope.open = function () {
+      var modal = new FileUploadModal($scope.fileUploadOptions);
+      modal.open();
+    };
+
+    $scope.closeAlert = function (index) {
+      $scope.alerts.splice(index, 1);
     };
   });
